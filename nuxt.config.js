c4f1f17d7abf445fc1c1e2ec88f7c6a3594ddf2a
神谷友樹 <jetree900@gmail.com>
2021-05-04 23:38:56 +0900

    eslintの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,17 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
 }
