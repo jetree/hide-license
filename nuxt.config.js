795f397e775ef3aab17d9f2b167f57a16dfd586c
神谷友樹 <jetree900@gmail.com>
2021-05-11 00:03:00 +0900

    netlifyのため追記

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,22 @@ export default {
         })
       }
     },
+    build: {
+      babel: {
+        presets({ isServer }) {
+          return [
+            [
+              require.resolve('@nuxt/babel-preset-app'),
+              // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
+              {
+                buildTarget: isServer ? 'server' : 'client',
+                corejs: { version: 3 }
+              }
+            ]
+          ]
+        }
+      }
+    }
   },
 
   env: {
