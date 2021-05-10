ac30d1ccb7a9cbb6a0e3f7d84df5918f1ae5ff3b
神谷友樹 <jetree900@gmail.com>
2021-05-11 00:15:46 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,22 +59,6 @@ export default {
         })
       }
     },
-    build: {
-      babel: {
-        presets({ isServer }) {
-          return [
-            [
-              require.resolve('@nuxt/babel-preset-app'),
-              // require.resolve('@nuxt/babel-preset-app-edge'), // For nuxt-edge users
-              {
-                buildTarget: isServer ? 'server' : 'client',
-                corejs: { version: 3 }
-              }
-            ]
-          ]
-        }
-      }
-    }
   },
 
   env: {
