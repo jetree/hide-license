ab1fddc25570d90338418d9eeab3646468e2af6b
神谷友樹 <jetree900@gmail.com>
2021-05-11 23:50:53 +0900

    説明書きを追加

---
+++ components/HowToUse.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div class="container">
+      <h6>step1</h6>
+      <div class="description">
+        <h2>車の写真を選択してください</h2>
+        <p>※複数選択できます</p>
+      </div>
+      <h6>step2</h6>
+      <div class="description">
+        <h2>「変換する」ボタンを押してください</h2>
+      </div>
+      <h6>step3</h6>
+      <div class="description">
+        <h2>ナンバープレートが塗りつぶされます</h2>
+        <p>※ナンバープレートを識別できた画像のみ処理されます</p>
+      </div>
+      <h6>step4</h6>
+      <div class="description">
+        <h2>「一括ダウンロード」ボタンが出現しますので保存してね</h2>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  width: 80%;
+  min-width: 200px;
+  min-height: 200px;
+  margin: 20px auto;
+  text-align: left;
+}
+.description {
+  margin-left: 2rem;
+}
+</style>
--- components/main.vue
+++ components/main.vue
@@ -1,5 +1,6 @@
 <template>
   <section>
+    <HowToUse />
     <div
       :class="{ onArea: onArea }"
       class="drop-area"
